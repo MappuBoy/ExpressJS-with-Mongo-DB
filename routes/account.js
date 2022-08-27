926906eab22bb2c74398a88b08ba5d87fa023b9c
mappuboy <pituvaraliyange@gmail.com>
2022-08-27 20:36:38 +0530

    account app creat

---
+++ routes/account.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const router=express.Router()
+
+router.get('/',(req,res)=>{
+
+})
+router.post('/',(req,res)=>{
+
+})
+router.put('/',(req,res)=>{
+
+})
+router.delete('/id',(res,req)=>{
+
+})
+router.get('/:id',(res,req)=>{
+
+})
+module.exports=router
